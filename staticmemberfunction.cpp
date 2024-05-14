fa670ff4c7d3daca256652b44817b4b6bf74ac7d
PannnTastic <pannntastic@gmail.com>
2024-05-14 18:15:47 +0700

    membuat static member function

--- staticmemberfunction.cpp
+++ staticmemberfunction.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 using namespace std;
+
+class mahasiswa
+{
+private:
+    static int nim;
+
+public:
+    int id;
+    string nama;
+
+    void setid();
+
+    void printall();
+
+    static void setnim(int pnim) { nim = pnim; }
+
+    static int getnim() { return nim; }
+};
