82be843f3d143e0ce79df58ce1890e43885c5ac9
PannnTastic <pannntastic@gmail.com>
2024-05-14 18:24:47 +0700

    memanggil static member value dan membuat output ke pengguna

--- staticmemberfunction.cpp
+++ staticmemberfunction.cpp
@@ -17,4 +17,21 @@ public:
     static void setnim(int pnim) { nim = pnim; }
 
     static int getnim() { return nim; }
+
+    mahasiswa (string pnama){
+        nama = pnama;
+        setid();
+    }
 };
+
+int mahasiswa::nim = 0;
+
+void mahasiswa ::setid(){
+    id = ++nim;
+}
+
+void mahasiswa :: printall(){
+    cout << "ID   : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+}
