833f72a48ffd64789416a27a90c5f9feaeab9034
PannnTastic <pannntastic@gmail.com>
2024-05-14 18:25:38 +0700

    membuat program utama

--- staticmemberfunction.cpp
+++ staticmemberfunction.cpp
@@ -35,3 +35,16 @@ void mahasiswa :: printall(){
     cout << "Nama : " << nama << endl;
     cout << endl;
 }
+
+int main (){
+    
+    mahasiswa mhs1("Lia Kurnia");
+    mahasiswa mhs2("Asroni");
+    mahasiswa mhs3("Andi Kurniawan");
+    mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printall();
+    mhs2.printall();
+    mhs3.printall();
+    mhs4.printall();
+}
