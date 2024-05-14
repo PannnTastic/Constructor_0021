45dfae3249297897dfd30151c19e93eb785d29c0
PannnTastic <pannntastic@gmail.com>
2024-05-14 17:37:14 +0700

    merapihkan sintaks

--- contohstaticmembervalue.cpp
+++ contohstaticmembervalue.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa {
-    public : 
-        static int nim;
-        int id;
-        string nama;
+class mahasiswa
+{
+public:
+    static int nim;
+    int id;
+    string nama;
 
-        void setid();
+    void setid();
 
-        void printall();
+    void printall();
 
-        mahasiswa (string pnama){
-            nama = pnama;
-        }
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+    }
 };
 
 int mahasiswa::nim = 113;
 
-void mahasiswa ::setid(){
+void mahasiswa ::setid()
+{
     id = ++nim;
 }
 
-void mahasiswa::printall(){
+void mahasiswa::printall()
+{
     cout << "ID   " << id << endl;
     cout << "Nama " << nama << endl;
     cout << endl;
 }
 
-int main (){
+int main()
+{
     mahasiswa mhs1("Lia Kurnia");
     mahasiswa mhs2("Asroni");
     mahasiswa mhs3("Andi Kurniawan");
